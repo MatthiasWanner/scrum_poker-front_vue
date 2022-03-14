3a7f11858663c0ad547365e9a0d475ab054c7a63
MatthiasWanner <matthias.wanner@gmail.com>
2022-03-14 22:30:32 +0100

    :construction: (wip) Create Title reusable component

---
+++ src/components/PageTitle/PageTitle.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps<{ title: string }>();
+</script>
+
+<template>
+  <section class="page-title">
+    <h1>{{ title }}</h1>
+  </section>
+</template>
+
+<style scoped lang="scss">
+.page-title {
+  width: 100%;
+  padding: 10px;
+  box-sizing: border-box;
+  font-size: x-large;
+}
+</style>
