7287d8d5864c3d5d8a19d99b5113fada81da5a3b
MatthiasWanner <matthias.wanner@gmail.com>
2022-03-14 23:45:18 +0100

    :sparkles: (feat) Mobile layout done. Import buttons component

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,10 +1,59 @@
 <script setup lang="ts">
 import PageTitle from '../components/PageTitle/PageTitle.vue';
 import homeContent from '../content/home.json';
+import Button from '../components/UI/Button/Button.vue';
+import { ref } from 'vue';
+
+const handleCreateParty = (state: boolean) => {
+  console.log('Create party');
+  isHidden.value = !state;
+};
+
+const handleJoinParty = (state: boolean) => {
+  console.log('Join party');
+  isHidden.value = !state;
+};
+
+const isHidden = ref<boolean>(true);
 </script>
 
 <template>
-  <PageTitle :title="homeContent.title" />
+  <div class="page-container">
+    <PageTitle :title="homeContent.title" />
+    <section class="page-main-section">
+      <Button
+        :text="homeContent.createParty"
+        :handle-click="() => handleCreateParty(isHidden)"
+      />
+      <span v-if="!isHidden">Be cool, it's not dev yet 😜</span>
+      <Button
+        :text="homeContent.joinParty"
+        :handle-click="() => handleJoinParty(isHidden)"
+      />
+    </section>
+  </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.page-container {
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+}
+
+.page-main-section {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  height: 60%;
+  position: relative;
+}
+
+span {
+  font-size: x-large;
+  font-weight: bold;
+  position: absolute;
+  text-align: center;
+  width: 100%;
+}
+</style>
