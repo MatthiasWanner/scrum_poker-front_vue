14a46acf89a3560209e62390f3ca8596b87b63bb
MatthiasWanner <matthias.wanner@gmail.com>
2022-03-14 22:32:25 +0100

    :bento: (page) Create Home page with only the title for the moment

---
+++ src/pages/Home.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import PageTitle from '../components/PageTitle/PageTitle.vue';
+import homeContent from '../content/home.json';
+</script>
+
+<template>
+  <PageTitle :title="homeContent.title" />
+</template>
+
+<style scoped lang="scss"></style>
