1b5b2b38c1300d6bd90ad53d38a044428456df9a
MatthiasWanner <matthias.wanner@gmail.com>
2022-03-17 21:53:20 +0100

    :sparkles: (layout) First page layout done

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
-import PageTitle from '../components/PageTitle/PageTitle.vue';
 import homeContent from '../content/home.json';
 import Button from '../components/UI/Button/Button.vue';
 import { ref } from 'vue';
+import Image from '../components/Image/Image.vue';
+import logo from '../../public/logo_rectangle.svg';
 
 const handleCreateParty = (state: boolean) => {
   console.log('Create party');
@@ -19,33 +20,47 @@ const isHidden = ref<boolean>(true);
 
 <template>
   <div class="page-container">
-    <PageTitle :title="homeContent.title" />
+    <Image :src="logo" :width="300" />
     <section class="page-main-section">
+      <h3 class="home-title">What do you want to do ?</h3>
       <Button
         :text="homeContent.createParty"
         :handle-click="() => handleCreateParty(isHidden)"
       />
-      <span v-if="!isHidden">Be cool, it's not dev yet 😜</span>
       <Button
         :text="homeContent.joinParty"
         :handle-click="() => handleJoinParty(isHidden)"
       />
+      <span v-if="!isHidden" class="temporaly-span"
+        >Be cool, it's not dev yet 😜</span
+      >
     </section>
   </div>
 </template>
 
 <style scoped lang="scss">
+.temporaly-span {
+  margin-top: 10px;
+  position: absolute;
+  bottom: 0;
+}
+
 .page-container {
   width: 100%;
   height: 100%;
   box-sizing: border-box;
+  position: relative;
+}
+
+.home-title {
+  font-size: x-large;
+  font-weight: bold;
 }
 
 .page-main-section {
   display: flex;
   flex-direction: column;
   justify-content: space-around;
-  height: 60%;
   position: relative;
 }
 
