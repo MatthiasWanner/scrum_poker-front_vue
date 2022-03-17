0072752ba2b5844933e1818b09f59e470807df3d
MatthiasWanner <matthias.wanner@gmail.com>
2022-03-17 23:02:09 +0100

    :bento: (components) New page will enable scrum to create a new game

---
+++ src/pages/CreateGame.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import PageTitle from '../components/PageTitle/PageTitle.vue';
+</script>
+
+<template>
+  <div class="page-container">
+    <PageTitle title="Create a new game" />
+    <section class="page-main-section">
+      <p>Awaiting Components</p>
+    </section>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.page-container {
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+  position: relative;
+}
+
+.page-main-section {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  position: relative;
+}
+</style>
