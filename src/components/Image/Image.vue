be300f185616d3fdb4d2b6851c606beaf1ed7528
MatthiasWanner <matthias.wanner@gmail.com>
2022-03-17 21:36:08 +0100

    :bento: (component) Image reusable component with some props to customize

---
+++ src/components/Image/Image.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps<{
+  src: string;
+  additionnalClasses?: string;
+  width?: number;
+  height?: number;
+}>();
+</script>
+
+<template>
+  <img
+    :src="src"
+    :class="`image ${additionnalClasses || ''}`"
+    :width="width || ''"
+    :height="height || ''"
+  />
+</template>
