fc1651e1166b3cf536e00c79c4c18fc96a12bdfe
MatthiasWanner <matthias.wanner@gmail.com>
2022-03-18 16:29:55 +0100

    :bento: (component) input reusable component (no style)

---
+++ src/components/Forms/TextInput/TextInput.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+interface IProps {
+  label: string;
+  labelClass?: string;
+  inputClass?: string;
+  fieldName: string;
+  disabled?: boolean;
+}
+
+defineProps<IProps>();
+</script>
+
+<template>
+  <div class="form-group field">
+    <input
+      :disabled="disabled || false"
+      :class="`form-field`"
+      type="text"
+      :placeholder="label"
+      :v-model="fieldName"
+    />
+    <label :for="fieldName" :class="`form-label`">{{ label }}</label>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.form-label {
+  display: none;
+}
+</style>
