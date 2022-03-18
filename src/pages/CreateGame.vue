cc3238b70c33c40168662f5af78102e2dcb846d4
MatthiasWanner <matthias.wanner@gmail.com>
2022-03-18 18:03:07 +0100

    :sparkles: (layout) Create game page layout done. Form connected to inputs

--- src/pages/CreateGame.vue
+++ src/pages/CreateGame.vue
@@ -1,11 +1,26 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import createGameContent from '../content/create_game.json';
+// eslint-disable-next-line import/no-unresolved
+import useForm from '../composables/useForm';
 import PageTitle from '../components/PageTitle/PageTitle.vue';
 import TextInput from '../components/Forms/TextInput/TextInput.vue';
-import createGameContent from '../content/create_game.json';
 import Button from '../components/UI/Button/Button.vue';
 
-const submitForm = () => {
-  console.log('Submitting form');
+interface IFormData {
+  gameName: string;
+  username: string;
+}
+
+const message = ref<string>('');
+
+const { handleSubmit, register } = useForm();
+
+const submitForm = ({ gameName, username }: IFormData) => {
+  if (gameName && username) {
+    return (message.value = `${username} want to create "${gameName}" Game. It will be possible when I have dev it 😂`);
+  }
+  return (message.value = `Please complete all fields 🤦‍♂️`);
 };
 </script>
 
@@ -13,17 +28,22 @@ const submitForm = () => {
   <div class="page-container">
     <PageTitle :title="createGameContent.title" />
     <section class="page-main-section">
-      <form @submit.prevent="submitForm">
+      <form @submit.prevent="handleSubmit(submitForm)">
         <TextInput
+          :register="register"
           field-name="gameName"
           :label="createGameContent.gameNameInputLabel"
         />
         <TextInput
+          :register="register"
           field-name="username"
           :label="createGameContent.usernameInputLabel"
         />
         <Button :text="createGameContent.createGameButtonLabel" type="submit" />
       </form>
+    </section>
+    <section class="page-main-section">
+      <p>{{ message }}</p>
     </section>
   </div>
 </template>
