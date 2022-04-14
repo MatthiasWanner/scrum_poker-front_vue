41faa2b738e7c77ada16884a27cb5caee3df9011
MatthiasWanner <matthias.wanner@gmail.com>
2022-04-14 22:19:09 +0200

    :sparkles: (feat) Create JoinGame page + externalize content

---
+++ src/pages/JoinGame.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import joinGameContent from '../content/join_game.json';
+// eslint-disable-next-line import/no-unresolved
+import useForm from '../composables/useForm';
+import PageTitle from '../components/PageTitle/PageTitle.vue';
+import TextInput from '../components/Forms/TextInput/TextInput.vue';
+import Button from '../components/UI/Button/Button.vue';
+
+interface IFormData {
+  gameId: string;
+  username: string;
+}
+
+const message = ref<string>('');
+
+const { handleSubmit, register } = useForm();
+
+const submitForm = ({ gameId, username }: IFormData) => {
+  if (gameId && username) {
+    return (message.value = `${username} want to join a game with the id "${gameId}". It will be possible when I have dev it 😂`);
+  }
+  return (message.value = `Please complete all fields 🤦‍♂️`);
+};
+</script>
+
+<template>
+  <div class="page-container">
+    <PageTitle :title="joinGameContent.title" />
+    <section class="page-main-section">
+      <form @submit.prevent="handleSubmit(submitForm)">
+        <TextInput
+          :register="register"
+          field-name="gameId"
+          :label="joinGameContent.gameIdInputLabel"
+        />
+        <TextInput
+          :register="register"
+          field-name="username"
+          :label="joinGameContent.usernameInputLabel"
+        />
+        <Button :text="joinGameContent.joinGameButtonLabel" type="submit" />
+      </form>
+    </section>
+    <section class="page-main-section">
+      <p>{{ message }}</p>
+    </section>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@use '../scss/layout';
+</style>
