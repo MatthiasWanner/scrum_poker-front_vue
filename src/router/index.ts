5d17c1631c28853871e723bb752aa909759f38e5
MatthiasWanner <matthias.wanner@gmail.com>
2022-04-14 22:23:45 +0200

    :sparkles: (feat) Add join game page into router. Connect it to home button

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-import CreateGame from '../pages/CreateGame.vue';
 import Home from '../pages/Home.vue';
+import CreateGame from '../pages/CreateGame.vue';
+import JoinGame from '../pages/JoinGame.vue';
 
 const routes = [
   { path: '/', component: Home },
   { path: '/create', component: CreateGame },
+  { path: '/join', component: JoinGame },
 ];
 
 export const router = createRouter({
