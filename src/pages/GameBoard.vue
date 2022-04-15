c37c46db200901a65bab04ca0a800c13c48aa9a7
MatthiasWanner <matthias.wanner@gmail.com>
2022-04-15 14:13:25 +0200

    :construction: (wip) New gameboard page added in router

---
+++ src/pages/GameBoard.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import PageTitle from '../components/PageTitle/PageTitle.vue';
+</script>
+<template>
+  <PageTitle title="Sprint #4" />
+</template>
+
+<style lang="scss" scoped></style>
