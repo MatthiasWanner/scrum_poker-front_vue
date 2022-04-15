296332cf8f6762faeb31633d4fbd5ea88b4e751a
MatthiasWanner <matthias.wanner@gmail.com>
2022-04-15 23:41:27 +0200

    :bento: (constants) Create temporary users list constant

---
+++ src/constants/users.constant.ts
@@ -0,0 +1,32 @@
+// eslint-disable-next-line import/no-unresolved
+import { IUser } from '../store';
+
+const users: IUser[] = [
+  {
+    id: '065998eb-1824-4f3c-99ec-21fb7d05622e',
+    username: '😀 John Doe',
+    role: 'DEVELOPER',
+  },
+  {
+    id: '9aee60e6-52b8-4db8-a200-f4810877dbc6',
+    username: '😱 Jane Doe',
+    role: 'DEVELOPER',
+  },
+  {
+    id: '38f701b3-dfb8-454e-94eb-87cfbabb6ea9',
+    username: '🥸 Telma',
+    role: 'DEVELOPER',
+  },
+  {
+    id: '66d4df20-0f28-4a01-9884-bbcc737d65b3',
+    username: '🤯 Louis',
+    role: 'DEVELOPER',
+  },
+  {
+    id: 'd76d7ca0-80b8-43e7-b661-6cecc442a243',
+    username: '😎 Arthur',
+    role: 'DEVELOPER',
+  },
+];
+
+export default users;
