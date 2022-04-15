98e5f6a313ef9dce612e3b87dbf91f005806d271
MatthiasWanner <matthias.wanner@gmail.com>
2022-04-15 22:04:23 +0200

    :sparkles: (feat) Game board wait users mobile layout done

---
+++ src/components/UserList/WaitingUsers.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import config from '../../content/config.json';
+import gameBoardContent from '../../content/game_board.json';
+import Button from '../UI/Button/Button.vue';
+
+const users = ref<string[]>([
+  'user1',
+  'user2',
+  'user3',
+  'user4',
+  'user5',
+  'user6',
+  'user7',
+  'user8',
+  'user9',
+  'user10',
+]);
+</script>
+<template>
+  <div :class="`${config.defaultTheme} wait-users-list`">
+    <p class="wait-users-list-title">{{ gameBoardContent.awaiting }}</p>
+    <ul>
+      <li class="user-item" v-for="user in users" :key="user">{{ user }}</li>
+    </ul>
+    <Button class="start-button" :text="gameBoardContent.start" />
+    <router-link to="/" class="cancel-link">{{
+      gameBoardContent.cancel
+    }}</router-link>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../../scss/purple-theme';
+@use '../../scss/light-theme';
+@use '../../scss/global';
+.wait-users-list {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  height: 100%;
+  width: 100%;
+  overflow: scroll;
+  border: none;
+  border-radius: 10px;
+  box-sizing: border-box;
+  margin-bottom: 20px;
+  padding: 10px;
+  &.purple {
+    color: purple-theme.$white;
+    background-color: purple-theme.$pastel-purple;
+  }
+}
+
+.wait-users-list-title {
+  font-size: x-large;
+  font-weight: bold;
+  margin-top: 5px;
+  margin-bottom: 5px;
+}
+
+.user-item {
+  text-align: start;
+  font-size: larger;
+}
+
+.start-button {
+  margin-top: auto;
+  margin-bottom: 10px;
+}
+
+.cancel-link {
+  color: purple-theme.$light-purple;
+  margin-bottom: 10px;
+}
+</style>
--- src/pages/GameBoard.vue
+++ src/pages/GameBoard.vue
@@ -1,8 +1,55 @@
 <script setup lang="ts">
 import PageTitle from '../components/PageTitle/PageTitle.vue';
+import gameBoardContent from '../content/game_board.json';
+import Button from '../components/UI/Button/Button.vue';
+import { ref } from 'vue';
+import Image from '../components/Image/Image.vue';
+import downDuo from '../../public/down_duo.svg';
+import copyLogo from '../../public/copy_logo.svg';
+import WaitingUsersList from '../components/UserList/WaitingUsers.vue';
+
+const gameId = ref('727643f9-5000-488a-8cff-c357e8bb08f9');
 </script>
 <template>
-  <PageTitle title="Sprint #4" />
+  <section class="gameboard-container">
+    <PageTitle title="Sprint #4" />
+    <p class="game-board-text">
+      #1 {{ gameBoardContent.clictoCopy }}
+      <Image class="down-arrow-duo" :src="downDuo" />
+    </p>
+    <Button :text="gameId" :secondary="true">
+      <Image :src="copyLogo" />
+    </Button>
+    <p class="game-board-text">#2 {{ gameBoardContent.sendId }}</p>
+    <WaitingUsersList />
+  </section>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.gameboard-container {
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  align-items: center;
+  height: 100%;
+}
+.game-board-text {
+  width: 100%;
+  display: flex;
+  font-size: x-large;
+  margin-bottom: 5px;
+}
+
+.down-arrow-duo {
+  animation: bounce 0.35s ease infinite alternate;
+}
+
+@keyframes bounce {
+  from {
+    transform: translateY(0);
+  }
+  to {
+    transform: translateY(-10px);
+  }
+}
+</style>
