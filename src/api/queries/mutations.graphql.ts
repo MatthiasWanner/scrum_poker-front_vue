2d6d2cea3500cb977ea28846d719baa28003814c
MatthiasWanner <matthias.wanner@gmail.com>
2022-04-30 00:45:24 +0200

    :bento: (resolvers) Update resolvers

--- src/api/queries/mutations.graphql.ts
+++ src/api/queries/mutations.graphql.ts
@@ -19,3 +19,40 @@ export const createGame = gql`
     }
   }
 `;
+
+export const joinGame = gql`
+  mutation joinGame($input: JoinGameInput!) {
+    joinGame(input: $input) {
+      game {
+        gameId
+        gameName
+        status
+        users {
+          userId
+          username
+          role
+          vote
+          hasVoted
+        }
+      }
+      redisResponse
+    }
+  }
+`;
+
+export const vote = gql`
+  mutation vote($input: PlayerVoteInput!) {
+    playerVote(input: $input) {
+      gameId
+      gameName
+      status
+      users {
+        userId
+        username
+        role
+        vote
+        hasVoted
+      }
+    }
+  }
+`;
