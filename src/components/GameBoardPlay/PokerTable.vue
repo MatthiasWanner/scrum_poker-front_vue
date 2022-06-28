ccac6739623f64ac45e6793e367b67d6ce57feb9
MatthiasWanner <matthias.wanner@gmail.com>
2022-06-28 18:14:56 +0200

    :recycle: (refactor) split play onboard into many components

--- src/components/GameBoardPlay/PokerTable.vue
+++ src/components/GameBoardPlay/PokerTable.vue
@@ -2,8 +2,14 @@
 // eslint-disable-next-line import/no-unresolved
 import { useAppStore } from '../../store';
 import config from '../../content/config.json';
+import UserEmoticon from './UserEmoticon.vue';
+import UserInfos from './UserInfos.vue';
+import UserResult from './UserResult.vue';
 
-const { game, user: userConnected } = useAppStore();
+const { game } = useAppStore();
+
+/* Temporary function that return a default emoji */
+const getEmoticon = () => '😂';
 </script>
 
 <template>
@@ -15,18 +21,9 @@ const { game, user: userConnected } = useAppStore();
         :key="user.userId"
         class="playing-user-item"
       >
-        <p class="user-emoticon">😎</p>
-        <div class="user-infos">
-          <span class="username">
-            {{ userConnected?.userId === user.userId ? 'You' : user.username }}
-          </span>
-          <span class="user-status">
-            {{ user.hasVoted ? '' : 'waiting...' }}
-          </span>
-        </div>
-        <div class="user-result">
-          {{ user.hasVoted ? user.vote : '?' }}
-        </div>
+        <UserEmoticon :emoticon="getEmoticon()" />
+        <UserInfos :user="user" />
+        <UserResult :vote="user.vote" />
       </li>
     </ul>
   </section>
@@ -71,39 +68,5 @@ const { game, user: userConnected } = useAppStore();
   margin: 0 0 20px; /* top | horizontal | bottom */
   padding: 0;
   line-height: 1;
-}
-
-.user-emoticon {
-  display: flex;
-  align-items: center;
-  margin: 0 5px 0 0; /* top | right | bottom | left */
-  font-size: xx-large;
-}
-
-.user-infos {
-  display: flex;
-  width: 100%;
-  flex-direction: column;
-  margin: 0;
-}
-
-.username {
-  font-weight: bold;
-  margin: 0;
-}
-
-.user-status {
-  font-size: medium;
-}
-
-.user-result {
-  display: flex;
-  justify-content: center;
-  box-sizing: border-box;
-  align-items: center;
-  width: 50px;
-  background-color: purple-theme.$dark-purple;
-  border-radius: 2px;
-  box-shadow: 2px 2px 5px 1px rgba(0, 0, 0, 0.3);
 }
 </style>
