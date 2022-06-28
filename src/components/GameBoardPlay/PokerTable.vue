104358c712af59ff0af8bc760222051abaf83980
MatthiasWanner <matthias.wanner@gmail.com>
2022-06-28 17:06:46 +0200

    :speech_balloon: (data) List all game users, add default emoji, result card

--- src/components/GameBoardPlay/PokerTable.vue
+++ src/components/GameBoardPlay/PokerTable.vue
@@ -1,9 +1,35 @@
 <script setup lang="ts">
+// eslint-disable-next-line import/no-unresolved
+import { useAppStore } from '../../store';
 import config from '../../content/config.json';
+
+const { game, user: userConnected } = useAppStore();
 </script>
 
 <template>
-  <section :class="`${config.defaultTheme} playing-users-container`"></section>
+  <section :class="`${config.defaultTheme} playing-users-container`">
+    <h3 class="game-name">{{ game.gameName }}</h3>
+    <ul>
+      <li
+        v-for="user in game.users"
+        :key="user.userId"
+        class="playing-user-item"
+      >
+        <p class="user-emoticon">😎</p>
+        <div class="user-infos">
+          <span class="username">
+            {{ userConnected?.userId === user.userId ? 'You' : user.username }}
+          </span>
+          <span class="user-status">
+            {{ user.hasVoted ? '' : 'waiting...' }}
+          </span>
+        </div>
+        <div class="user-result">
+          {{ user.hasVoted ? user.vote : '?' }}
+        </div>
+      </li>
+    </ul>
+  </section>
 </template>
 
 <style lang="scss" scoped>
