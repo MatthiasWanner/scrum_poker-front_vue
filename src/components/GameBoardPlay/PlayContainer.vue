cfbcd35b6f1ee3a1613b8bbdddfe847cb6582f3e
MatthiasWanner <matthias.wanner@gmail.com>
2022-06-29 22:09:48 +0200

    :construction: (subscriptions) Watch ws events playing game to manage state

--- src/components/GameBoardPlay/PlayContainer.vue
+++ src/components/GameBoardPlay/PlayContainer.vue
@@ -1,6 +1,55 @@
 <script setup lang="ts">
+import {
+  useSubscribeGameSubscription,
+  // eslint-disable-next-line import/no-unresolved
+} from '../../api/generated';
+// eslint-disable-next-line import/no-unresolved
+import { useAppStore } from '../../store';
+import { watch } from 'vue';
 import PokerTable from './PokerTable.vue';
 import UserCards from './UserCards.vue';
+// eslint-disable-next-line import/no-unresolved
+import { router } from '../../router';
+
+const {
+  user: userConnected,
+  game: { gameId },
+  isVoteSecret,
+  setPlayerVoteStatus,
+  resetPlayersVotes,
+  revealPlayersVotes,
+} = useAppStore();
+
+const { result: gameSubscription } = useSubscribeGameSubscription({
+  gameId,
+});
+
+watch(gameSubscription, (data) => {
+  if (data) {
+    const events = data.playingGame;
+
+    for (const event of events) {
+      switch (event.__typename) {
+        case 'GameVoteEvent': {
+          setPlayerVoteStatus(event.voteEventPayload, true);
+          break;
+        }
+        case 'GameStatusEvent': {
+          router.push('/');
+          break;
+        }
+        case 'GameResetEvent': {
+          resetPlayersVotes();
+          break;
+        }
+        case 'GameRevealVoteEvent': {
+          revealPlayersVotes(event.revealVotesPayload);
+          break;
+        }
+      }
+    }
+  }
+});
 </script>
 <template>
   <PokerTable />
