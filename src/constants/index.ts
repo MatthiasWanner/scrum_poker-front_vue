302e08a9569d1ac50f8daaa8afed80fb3877631f
MatthiasWanner <matthias.wanner@gmail.com>
2022-07-05 14:02:59 +0200

    :rotating_light: (ts) Delete useless user constants (build error role type)

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,2 @@
 // eslint-disable-next-line import/no-unresolved
-export { default as users } from './users.constant';
-
-// eslint-disable-next-line import/no-unresolved
 export { default as voteCards } from './vote-cards.constant';
